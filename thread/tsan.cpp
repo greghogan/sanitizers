7c317d3d82c8b99f2f6dfcc8408cc3da6c5883e3
Greg Hogan <code@greghogan.com>
2020-11-03 12:55:48 -0500

    Demo for thread sanitizer

---
+++ thread/tsan.cpp
@@ -0,0 +1,20 @@
+#include <pthread.h>
+
+/*
+ * source from https://clang.llvm.org/docs/ThreadSanitizer.html
+ */
+
+int Global;
+
+void *Thread1(void *x) {
+    Global = 42;
+    return x;
+}
+
+int main() {
+    pthread_t t;
+    pthread_create(&t, NULL, Thread1, NULL);
+    Global = 43;
+    pthread_join(t, NULL);
+    return Global;
+}
